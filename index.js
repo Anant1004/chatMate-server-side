7d0c3e7af3146cf0d653ac3c5529c1ea35e6322a
Anant kumar <anant1042005@gmail.com>
2024-11-11 22:34:35 +0530

    morgan added

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv'
 import authRoutes from './routes/authRoutes.js'
 import connectToDb from './connections/db.js';
+import morgan from 'morgan';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.use(morgan('dev'));
 app.use('/api/auth',authRoutes);
 
 app.get('/', (req, res) => {
