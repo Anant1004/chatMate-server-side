6ec9431a65014090f0c18b540ac3af6e6cf11c25
Anant kumar <anant1042005@gmail.com>
2024-11-13 13:46:38 +0530

    authenticatoin mmiddleware and controllers for the friend request accepting

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import authRoutes from './src/routes/authRoutes.js'
+import friendsRoutes from './src/routes/friendsRoutes.js'
 import connectToDb from './src/connections/db.js';
 import morgan from 'morgan';
+import authenticate from './src/middlewares/authorization.js';
 
 dotenv.config();
 const app = express();
@@ -12,6 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(morgan('dev'));
 app.use('/api/auth',authRoutes);
+app.use('/api/friend-requests',authenticate,friendsRoutes);
+
 
 app.get('/', (req, res) => {
     res.send('Hello World !!');
