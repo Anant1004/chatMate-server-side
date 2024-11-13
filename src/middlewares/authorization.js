546fcc951bb392e730c4cec451a68fd8799d6e88
Mantu Kumar <mantu911711@gmail.com>
2024-11-14 00:33:18 +0530

    updated the code

--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -1,14 +1,20 @@
 import jwt from 'jsonwebtoken';
+import User from '../models/userModel.js'
 const JWT_SECRET = process.env.JWT_SECRET
 
-const authenticate = (req, res, next) => {
+const authenticate = async(req, res, next) => {
     const token = req.cookies?.token;
     if (!token) {
         return res.status(401).json({ error: 'Access denied. No token provided.' });
     }
     try {
         const decoded = jwt.verify(token, JWT_SECRET); 
-        req.user = decoded;
+        const user=await User.findById(decoded?.userId);
+        console.log(user)
+        if (!user) {
+            return res.status(404).json({ error: 'User not found or Invalid Token.' });
+        }
+        req.user = user;
         next();
     } catch (error) {
         res.status(400).json({ error: 'Invalid token.' });
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 const router = express.Router();
 import {signupUser, loginUser, logoutUser} from '../controllers/authControllers.js';
+import authenticate from '../middlewares/authorization.js';
 
 router.post('/signup',signupUser);
 router.post('/login',loginUser);
-router.post('/logout',logoutUser);
+router.post('/logout',authenticate,logoutUser);
 
 
 export default router
