eb5a8643ce8e7aa4189cc45c2a7a186d0b6d3b5a
Anant kumar <anant1042005@gmail.com>
2024-11-14 17:32:32 +0530

    removed the dotenv from authcontrollers.js file

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,9 +1,6 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import User from '../models/userModel.js';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 
 const signupUser = async (req, res) => {
