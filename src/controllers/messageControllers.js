b693881f783cbe53f7267b4a9adc6f773719f1c7
Anant kumar <anant1042005@gmail.com>
2024-11-24 19:40:12 +0530

    aded some rough changes

--- src/controllers/messageControllers.js
+++ src/controllers/messageControllers.js
@@ -5,32 +5,32 @@ import User from '../models/userModel.js';
 // Send Message to User
 const sendMessage = async (req, res) => {
     try {
-        const { targetUser,targetGroupId, text } = req.body;      // either targetUser or targetGroup should be sent from frontend
-        if (targetUser && targetGroupId){
+        const { targetUser, targetGroupId, text } = req.body; // either targetUser or targetGroup should be sent from frontend
+        if (targetUser && targetGroupId) {
             return res.status(400).json({ message: 'Both targetUser and targetGroup cannot be sent at the same time.' });
         }
-        if(!targetUser && !targetGroupId){
+        if (!targetUser && !targetGroupId) {
             return res.status(400).json({ message: 'Either targetUser or targetGroup is required.' });
         }
-        if(targetUser){
-            const user=req.user;
-            const receiver = await User.findOne({userName:targetUser});
-            if(!receiver){
+        if (targetUser) {
+            const user = req.user;
+            const receiver = await User.findOne({ userName: targetUser });
+            if (!receiver) {
                 return res.status(400).json({ message: `${receiver} does not exist.` });
             }
-            if(!user.friends.includes(receiver._id)){
+            if (!user.friends.includes(receiver._id)) {
                 return res.status(400).json({ message: `${targetUser} is not a friend of ${user.userName}.` });
             }
-            const chat=user.chats.find(chat=>chat.chatWith===targetUser);
-            await Message.findByIdAndUpdate(chat.messages,{
-                $push:{text:{content:text,sender:user._id,readBy:[]}}
+            const chat = user.chats.find(chat => chat.chatWith === targetUser);
+            await Message.findByIdAndUpdate(chat.messages, {
+                $push: { text: { content: text, sender: user._id, readBy: [] } }
             });
-        }else{
-            
+        } else {
+
         }
-        return res.status(201).json({ message:'message sent successfully. '});
+        return res.status(201).json({ message: 'message sent successfully. ' });
     } catch (error) {
-        console.log('error while sending message',error)
+        console.log('error while sending message', error)
         res.status(500).json({ error: error.message });
     }
 };
@@ -38,12 +38,13 @@ const sendMessage = async (req, res) => {
 // Get Messages with User
 const getMessagesWithUser = async (req, res) => {
     try {
-        const user=req.user;
-        const chat=user.chats.find(chat=>chat.chatWith===req.params.userId);
-        if(!chat){
+        const user = req.user;
+        const chat = user.chats.find(chat => chat.chatWith === req.params.userId);
+        console.log("this is the chat", chat)
+        if (!chat) {
             return res.status(400).json({ message: 'No chat found.' });
         }
-        const messages=await Message.findById(chat.messages);
+        const messages = await Message.findById(chat.messages);
         res.status(200).json(messages.text);
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -51,16 +52,16 @@ const getMessagesWithUser = async (req, res) => {
 };
 
 // Get Messages in a Group
-const getGroupMessages=async(req,res)=>{
+const getGroupMessages = async (req, res) => {
     try {
-        const group=await Group.findById(req.params.groupId);
-        if(!group){
+        const group = await Group.findById(req.params.groupId);
+        if (!group) {
             return res.status(400).json({ message: 'Group does not exist.' });
         }
-        if(!group.members.includes(req.user._id)){
+        if (!group.members.includes(req.user._id)) {
             return res.status(400).json({ message: 'User is not a member of this group.' });
         }
-        const messages=await Message.find(group.messages);
+        const messages = await Message.find(group.messages);
         res.status(200).json(messages.text);
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -70,10 +71,10 @@ const getGroupMessages=async(req,res)=>{
 // Mark Message as Read
 const setReadBy = async (req, res) => {
     try {
-        await Message.findByIdAndUpdate(req.body.messageId,{
+        await Message.findByIdAndUpdate(req.body.messageId, {
             $set: { "text.$[].isSeen": true }
         },);
-        res.status(201).json({ message:'Message read successfully.' });
+        res.status(201).json({ message: 'Message read successfully.' });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
