4fb3015d27c3f08750cf25b3b57671ea091d7b31
Anant kumar <anant1042005@gmail.com>
2024-12-21 13:53:55 +0530

    removed the socket.io logic

--- src/sockets/socketHandlers.js
+++ src/sockets/socketHandlers.js
@@ -1,18 +1,6 @@
 const handleSocketEvents = (io) => {
     io.on('connection', (socket) => {
         console.log('A user connected:', socket.id);
-        socket.on('sendMessage', (message) => {
-            const { text, targetUser} = message;
-            // message dega
-            console.log(`Message from ${targetUser} is ${text}`);
-        });
-
-        // Join a specific room
-        socket.on('joinRoom', (roomId) => {
-            socket.join(roomId);
-            console.log(`${socket.id} joined room: ${roomId}`);
-        });
-
         // Handle disconnect
         socket.on('disconnect', () => {
             console.log('A user disconnected:', socket.id);
