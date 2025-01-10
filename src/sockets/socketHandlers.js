28aacf3c52139db3a06c32419c2e65c8d31753f0
Mantu Kumar <mantu911711@gmail.com>
2025-01-11 00:21:34 +0530

    socket io added

--- src/sockets/socketHandlers.js
+++ src/sockets/socketHandlers.js
@@ -4,18 +4,24 @@ const handleSocketEvents = (io) => {
     io.on('connection', (socket) => {
         console.log(`A user connected: ${socket.id}`);
 
-        socket.on('userId', (id) => {
-            onlineUsers[socket.id] = id;
+        socket.on('userName', (userName) => {
+            onlineUsers[socket.id] = userName;
             io.emit('onlineUsers', onlineUsers);
-            console.log("Updated online users:", onlineUsers);
+            console.log("after connection : ",onlineUsers);
+            
         });
+
+        socket.on('sendMessage', (payload) => console.log(payload.content));
 
         // Handle user disconnection
         socket.on('disconnect', () => {
             console.log(`A user disconnected: ${socket.id}`);
+            console.log("Before disconnect : ",onlineUsers);
+            
             delete onlineUsers[socket.id];
             io.emit('onlineUsers', onlineUsers);
-            console.log("Updated online users after disconnection:", onlineUsers);
+            console.log("After disconnect : ",onlineUsers);
+            
         });
     });
 };
